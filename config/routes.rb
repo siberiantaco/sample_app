ba3c5b523acea2496665c0bf8ed48438e80f99cb
siberian_taco <reg.rsaiadian@gmail.com>
2015-01-18 16:08:41 +0000

    chapter 4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contacts'
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
