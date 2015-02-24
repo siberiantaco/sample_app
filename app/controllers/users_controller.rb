a675997a1e79a70df1927d39cd10a811418757a6
siberian_taco <reg.rsaiadian@gmail.com>
2015-02-24 15:19:53 +0000

    excer 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,7 @@ private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+
     end
     
     def logged_in_user
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
@@ -10,6 +14,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contacts_path
     assert_select "a[href=?]", contacts_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links for logged user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contacts_path
+    assert_select "a[href=?]", contacts_path
+    assert_select "a[href=?]", users_path
+    
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
     get signup_path
     assert_select "title", full_title("Sign up")
   end
