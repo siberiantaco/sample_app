bc5fe9e0e901f2689f3a34acc6682ea287d895de
siberian_taco <reg.rsaiadian@gmail.com>
2015-03-12 06:56:02 +0000

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   before_save { email.downcase! }
   before_create :create_activation_digest
@@ -42,10 +42,27 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     UserMailer.account_activation(self).deliver_now
   end
   
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
   private
   
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(self.activation_token)
+    #no update_attribute cos before_create
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
