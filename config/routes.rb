e195ab8fa30ed8b2cb9b5a9c995a54bdbc7737b5
siberian_taco <reg.rsaiadian@gmail.com>
2015-03-27 16:21:05 +0000

    Finally!

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,15 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,  only: [:create, :destroy]
+  resources :relationships,  only: [:create, :destroy]
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
